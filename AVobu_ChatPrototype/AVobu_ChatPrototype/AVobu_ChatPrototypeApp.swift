0d4ee7f03a863496f75a295df0e8df2b725ff691
Aditya <avobu29@student.kis.or.kr>
2026-04-23 09:00:48 +0900

    Create Xcode ChatPrototype Project

---
+++ AVobu_ChatPrototype/AVobu_ChatPrototype/AVobu_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVobu_ChatPrototypeApp.swift
+//  AVobu_ChatPrototype
+//
+//  Created by Aditya Vobu on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct AVobu_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
